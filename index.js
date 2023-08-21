4fcb2335102973832cb863230b275244639f0a00
Gabriel Baptista <biellz1221@gmail.com>
2023-08-21 17:27:07 -0300

    testando de novo

--- index.js
+++ index.js
@@ -164,9 +164,10 @@ app.command("/qrcode", async ({ command, ack, say }) => {
 			say("Por favor coloque o conteúdo do QR Code");
 		}
 
-		// const user = await got().json();
+		const user = await got(`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${content}`);
 
-		say(`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${content}`);
+		// say(`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${content}`);
+		say(user);
 	} catch (error) {
 		console.error("======================== ERROR =========================");
 		console.error(error);
